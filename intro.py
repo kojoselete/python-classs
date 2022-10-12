213057d334f0629757c0aff65ed95792da6c2563
Selete69 <64495468+Selete69@users.noreply.github.com>
2022-10-12 09:08:10 +0000

    python assignment

---
+++ intro.py
@@ -0,0 +1,9 @@
+  
+firstNumber = int(input("enter first number"))
+secondNumber = int(input("enter second number"))
+
+def performAddition(firstNumber,secondNumber):
+    sum = firstNumber+secondNumber
+    return sum
+
+print(performAddition(firstNumber,secondNumber))
